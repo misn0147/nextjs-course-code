1e3ac23999ba50bf67ebdf5b86be3572ecf337f0
M Natoli <natoli.missy@gmail.com>
2022-03-14 10:02:09 -0500

    completed events program

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,9 +2,17 @@ import Link from "next/Link";
 import styles from "./button.module.css";
 
 export default function Button(props) {
+    if (props.link) {
+        return (
+            <Link href={props.link}>
+                <a className={styles.btn}>{props.children}</a>
+            </Link>
+        );
+    }
+
     return (
-        <Link href={props.link}>
-            <a className={styles.btn}>{props.children}</a>
-        </Link>
+        <button className={styles.btn} onClick={props.onClick}>
+            {props.children}
+        </button>
     );
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,21 @@
-import { getAllEvents } from '../../dummy-data';
-import EventList from '../../components/events/event-list';
+import { getAllEvents } from "../../dummy-data";
+import { useRouter } from 'next/router';
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
     const events = getAllEvents();
+    const router = useRouter();
+
+    function findEventsHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    }
 
     return (
         <div>
-            <EventList items={events}/>
+            <EventsSearch onSearch={findEventsHandler}/>
+            <EventList items={events} />
         </div>
     );
 }
