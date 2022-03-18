0fa96d7ca49c9c27857c31adaee4be569001dd77
M Natoli <natoli.missy@gmail.com>
2022-03-18 11:50:33 -0500

    added server side data fetching

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,11 @@
-import { getAllEvents } from "../../dummy-data";
-import { useRouter } from 'next/router';
+import { getAllEvents } from "../../helpers/api-util";
+import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
-function AllEventsPage() {
-    const events = getAllEvents();
+function AllEventsPage(props) {
     const router = useRouter();
+    const { events } = props;
 
     function findEventsHandler(year, month) {
         const fullPath = `/events/${year}/${month}`;
@@ -14,10 +14,21 @@ function AllEventsPage() {
 
     return (
         <div>
-            <EventsSearch onSearch={findEventsHandler}/>
+            <EventsSearch onSearch={findEventsHandler} />
             <EventList items={events} />
         </div>
     );
+}
+
+export async function getStaticProps() {
+    const events = await getAllEvents();
+
+    return {
+        props: {
+            events: events,
+        },
+        revalidate: 60
+    };
 }
 
 export default AllEventsPage;
